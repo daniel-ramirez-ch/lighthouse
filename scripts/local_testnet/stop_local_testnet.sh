d3a5bb129c78b5e8782c7460ee1fd43842506234
Daniel Ramirez <hi@danielrachi.com>
2024-01-13 12:07:32 -0500

    Fix stop_local_testnet to also kill stale pids

--- scripts/local_testnet/stop_local_testnet.sh
+++ scripts/local_testnet/stop_local_testnet.sh
@@ -6,5 +6,8 @@ set -Eeuo pipefail
 source ./vars.env
 
 PID_FILE=$TESTNET_DIR/PIDS.pid
+PID_FILE_STALE=$TESTNET_DIR/PIDS_STALE.pid
 ./kill_processes.sh $PID_FILE
+./kill_processes.sh $PID_FILE_STALE
 rm -f $PID_FILE
+rm -f $PID_FILE_STALE
